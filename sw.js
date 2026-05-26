6a4a385ee20f0313975fe3fc4ca94c5a4bc90ed8
sinarmediaproduction-tech <sinarmedia.production@gmail.com>
2026-05-26 14:19:45 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Service Worker Sederhana untuk Syarat PWA
+self.addEventListener('fetch', function(event) {
+    // Membiarkan aplikasi mengambil data secara online/offline dari localStorage
+});
